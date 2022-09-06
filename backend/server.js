65351ba686a7b05b676349a14ea9551f02799559
Your Name <you@example.com>
2022-09-06 17:03:55 +0200

    asd

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.listen(3002)
+app.listen(3002, () => {
+    console.log('Server listening on Port 3002!')
+})
 
 app.get('/', (req, res) => {
     res.status(500).send('Hi')
