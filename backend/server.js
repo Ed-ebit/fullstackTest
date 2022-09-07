baeca463a5af9d3baad90dfb7b5e6e9afea0f6c3
Your Name <you@example.com>
2022-09-07 15:46:18 +0200

    gfhj

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'testuser',
+    password : 'Pa55w0rd',
+    database:'voteschema'
+});
+
+
 
 app.listen(3002, () => {
     console.log('Server listening on Port 3002!')
@@ -7,4 +16,12 @@ app.listen(3002, () => {
 
 app.get('/', (req, res) => {
     res.status(500).send('Hi')
+    // connection.connect();
+    connection.query(
+        'SELECT * FROM voteschema.bundeslaender where idbundeslaender = 2', function(err, rows, fields)
+    { if (err) console.log('Na toll!\n'+err);
+    // res.send('ho');
+    // connection.end();
+    })
+    
 })
