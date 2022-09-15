44e00fc432d1b664fbac008145c343a0806297fe
Your Name <you@example.com>
2022-09-15 16:57:54 +0200

    sad

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -9,17 +9,17 @@ const connection = mysql.createConnection({
   database:'voteschema'
 });
 
-router.get('/datenbankabfrage',(req, res)=> {
-    connection.query('SELECT * FROM bundeslaender where idbundeslaender = 2', function(err, rows, fields) 
-{
-  if (err) throw err;
+// router.get('/datenbankabfrage',(req, res)=> {
+//     connection.query('SELECT * FROM bundeslaender where idbundeslaender = 2', function(err, rows, fields) 
+// {
+//   if (err) throw err;
 
-  console.log(rows[0]);
-//   console.log(fields[0]);
-res.send(rows[0]);
-// res.end(JSON.stringify(str));
-});
-})
+//   console.log(rows[0]);
+// //   console.log(fields[0]);
+// res.send(rows[0]);
+// // res.end(JSON.stringify(str));
+// });
+// })
 
 router.get('/datenbankabfrage/:item',(req, res)=> {
   // console.log(req.params.item);
--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import { DownOutlined } from '@ant-design/icons';
 import { Dropdown, Menu, message, Space } from 'antd';
@@ -6,31 +6,65 @@ import 'antd/dist/antd.css';
 
 
 const onClick = ({ key }) => {
-    message.info(`Click on ${key}`);
+  message.info(`Click on ${key}`);
+};
+
+function GetMenuCountries () {
+
+
+  useEffect( () => {
+      fetchCountries();
+  }, []);
+
+  const [countries, setCountries] = useState([]);
+
+  const fetchCountries = async() => {
+      const data = await fetch('/datenbankabfrage/bundeslaender_name'); // hier prog für mögliche abfragen des Dropdowns! - FE 'zieht' sich die Daten die es wünscht!
+      const countries = await data.json();
+      console.log(data)
+      // setCountries(countries);
+      // console.log(countries)
+      // console.log(test)
+      const countryArray = countries.map(country => country.bundeslaender_name);
+      console.log(countryArray)
+
   };
-  
-  const menu = (
+ return (
     <Menu
       onClick={onClick}
-      items={[
-        {
-          label: '{blKey}',
-          key: '{blKey}',
-        },
-        {
-          label: '2nd menu item',
-          key: '2',
-        },
-        {
-          label: '3rd menu item',
-          key: '3',
-        },
-      ]}
-    />
+      // items = {countries.bundeslaender_name}
+      items = {countryArray}
+      // items = {Object.keys(countries).map((key) => {return(
+      // {[{label: countries[key], key: countries[key]},]})})} 
+      
+      // {countries.forEach(elements => {
+      //   {label: {elements.key}}
+      //   {label: {elements.key}}
+
+      // })}
+      // items={[
+      //   {
+      //     label: '{blKey}',
+      //     key: '{blKey}',
+      //   },
+      //   {
+      //     label: '2nd menu item',
+      //     key: '2',
+      //   },
+      //   {
+      //     label: '3rd menu item',
+      //     key: '3',
+      //   },
+      // ]}
+            // {Object.keys(countries).map((key) => {return(
+      // <option key={countries[key]} value={countries[key]}>x</option>)})}
+     >
+
+    </Menu>
   );
-  
+}
   const App = () => (
-    <Dropdown overlay={menu}>
+    <Dropdown overlay={GetMenuCountries()}>
       <a onClick={(e) => e.preventDefault()}>
         <Space>
           Hover me, Click menu item
