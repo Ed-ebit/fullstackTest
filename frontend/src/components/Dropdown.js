6da40499d58fd21b74849cc86fac8c575e6c5a01
Your Name <you@example.com>
2022-09-22 16:40:03 +0200

    ghh

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,8 +1,9 @@
 import {React, useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import { DownOutlined } from '@ant-design/icons';
-import { Dropdown, Menu, message, Space } from 'antd';
+import { Dropdown, Menu, message, Space, Select } from 'antd';
 import 'antd/dist/antd.css';
+const {Option} = Select;
 
 
 
@@ -22,59 +23,10 @@ import 'antd/dist/antd.css';
 // }
 
 
+  const App = () => (
+      <Select >
 
-function GetMenuItems (table, item) {
-  console.log(item)
-
-  useEffect( () => {
-      fetchItems();
-  }, []);
-
-  const [items, setItems] = useState([]);
-  const [activeKey, setActiveKey] = useState(null);
-
-  const fetchItems = async() => {
-      const data = await fetch(`/datenbankabfrage/${table}/${item}`); // hier prog für mögliche abfragen des Dropdowns! - FE 'zieht' sich die Daten die es wünscht!
-      const items = await data.json();
-      console.log(items)
-      const itemsArray = Object.values(items).map((value) => {
-        console.log(value[item]);
-        return(
-        <Menu.Item key = {value[item]}>{value[item]}</Menu.Item>
-        )
-        })
-      setItems(itemsArray)
-  };
-useEffect(()=>{
-  console.log(activeKey);
-}, [activeKey]);
-  const onClick = ({ key }) => {
-    message.info(`${key}`);
-     console.log (key)
-     setActiveKey(key);
-    // chosenCountry(key)
-    // export const chosenCountry = `${key}`;
-    // SetClickedItem(key);
-  };
-  
- return (
-    <Menu
-      onClick={onClick}
-     >
-      {items}
-    </Menu>
- );
-}
-  const App = (props) => (
-    <Dropdown overlay={GetMenuItems(props.table, props.item)}>
-      {/* <a onClick={(e) => e.preventDefault()}> */}
-        <Space>
-          Bitte {props.name} wählen
-          <DownOutlined />
-        </Space>
-      {/* </a> */}
-    </Dropdown>
-    
+      </Select>
   );
   
   export default App;
--- frontend/src/components/pages/Datenbankabfrage.js
+++ frontend/src/components/pages/Datenbankabfrage.js
@@ -1,7 +1,9 @@
 import {React, useEffect, useState} from 'react';
 // import {Link} from 'react-router-dom';
+import { Menu, message, Space, Select } from 'antd';
 import 'antd/dist/antd.css';
 import Dropdown from '../Dropdown';
+const {Option} = Select;
 
 function Datenbankabfrage () {
     // console.log(Dropdown.chosenCountry)
@@ -18,19 +20,59 @@ function Datenbankabfrage () {
     //     setItems(data);
     //      console.log(items) 
     // };
+    
+
+function GetMenuItems (table, item) {
+    console.log(item)
+  
+    useEffect( () => {
+        fetchItems();
+    }, []);
+  
+    const [items, setItems] = useState([]);
+    const [activeKey, setActiveKey] = useState(null);
+  
+    const fetchItems = async() => {
+        const data = await fetch(`/datenbankabfrage/${table}/${item}`); // hier prog für mögliche abfragen des Dropdowns! - FE 'zieht' sich die Daten die es wünscht!
+        const items = await data.json();
+        console.log(items)
+        const itemsArray = Object.values(items).map((value) => {
+          console.log(value[item]);
+          return(
+          <Option value = {value[item]}>{value[item]}</Option>
+          )
+          })
+        setItems(itemsArray)
+        
+    };
+    console.log(items)
+    return items;
+//   useEffect(()=>{
+//     console.log(activeKey);
+//   }, [activeKey]);
+//     const onClick = ({ key }) => {
+//       message.info(`${key}`);
+//        console.log (key)
+//        setActiveKey(key);
+//       // chosenCountry(key)
+//       // export const chosenCountry = `${key}`;
+//       // SetClickedItem(key);
+//     };
+//    return (
+//       <Menu
+//         onClick={onClick}
+//        >
+//         {items}
+//       </Menu>
+//    );
+  }
 
     
     return (
         <section>
-            <Dropdown name= 'Bundesland' table = 'bundeslaender' item ='bundeslaender_name' />
-            <Dropdown name='Haustyp'table ='haustyp' item='haustyp_name'/>
-            {/* <div>
-                {Object.keys(items).map((key) => {
-                    return(
-                        <p>{key}: {items[key]}</p>
-                    )
-                })}
-            </div> */}
+            <Dropdown defaultValue='Bundesalnd wählen'>
+                {GetMenuItems('bundeslaender','bundeslaender_name')} 
+            </Dropdown>
         </section>
     );
 }
